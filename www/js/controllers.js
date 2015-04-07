67e94f3e45ec96b4012c30b9414ea365d2dca0fd
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-04-07 12:58:08 +0530

    zooming for profile image and images for lifehack users functionlity implemented.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -158,10 +158,38 @@ angular.module('starter.controllers', [])
     };
   })
 
-  .controller('ProfileCtrl', function ($scope, $stateParams) {
-    console.log('in ProfileCtrl')
+  .controller('ProfileCtrl', function ($scope, $stateParams, $ionicModal) {
+    console.log('in ProfileCtrl');
+
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.profile_image = 'http://lorempixel.com/250/400/sports';
+    });
+
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
+
   })
-  .controller('ProfileLifeHacksCtrl', function ($scope, $rootScope, $stateParams, $http, serviceUrls, serverCall) {
+  .controller('ProfileLifeHacksCtrl', function ($scope, $rootScope, $stateParams, $http, $ionicModal, serviceUrls, serverCall) {
 
     $scope.lifehackedData;
     var headers_obj = {
@@ -184,8 +212,8 @@ angular.module('starter.controllers', [])
     };
 
     serverCall
-     .apiDataCall(config)
-     .then(lifeHackDataSuccessCallback,lifeHackDataErrorCallback)
+      .apiDataCall(config)
+      .then(lifeHackDataSuccessCallback, lifeHackDataErrorCallback)
 
 
   })
